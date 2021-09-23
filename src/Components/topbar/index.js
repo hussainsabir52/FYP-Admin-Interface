54b185e5d99db97408918a62228bfe43eef968e3
Mustaali-Hussain <mustaali20may@gmail.com>
2021-09-23 13:15:31 +0500

    Starting up

---
+++ src/Components/topbar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Topbar() {
+  return (
+    <div className="border-2 w-100 h-50">
+      <div>
+        <div>left</div>
+        <div>Right</div>
+      </div>
+    </div>
+  );
+}
