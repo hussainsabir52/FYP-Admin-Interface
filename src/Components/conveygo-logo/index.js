bb8d085872cc1cbe9f8bce217708df1996f97924
Mustaali-Hussain <mustaali20may@gmail.com>
2021-09-23 19:39:13 +0500

    Topbar front end view, no functionality yet.

---
+++ src/Components/conveygo-logo/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ConveygoLogo() {
+    return (
+        <div>
+          <span className="font-bold text-3xl text-yellow-400 cursor-pointer">
+            ConveyGo
+          </span>
+        </div>
+    )
+}
